ac7d3eac64e951e73e34ead2023f42791e81ce51
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-01-09 12:06:05 +0000

    install-jenkins-awsLinux.sh

---
+++ install-jenkins-awsLinux.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo yum update –y
+
+R='\033[0;31m'    #Y='\033[1;32m'  #B='\033[0;34m'
+G='\033[0;32m'
+
+yum install -y httpd.x86_64
+
+systemctl start httpd.service
+
+systemctl enable httpd.service
+
+sudo yum install -y wget
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum upgrade
+
+sudo dnf install java-17-amazon-corretto -y
+
+sudo yum install jenkins -y
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+echo -e "${G}The Amindmin Password is : "
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+sudo systemctl status jenkins

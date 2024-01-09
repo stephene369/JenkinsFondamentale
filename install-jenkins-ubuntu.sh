2e7daf6c294fc582e337449ad6e15ea776a7ff18
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-01-09 20:32:47 +0000

    Create install-jenkins-ubuntu.sh

---
+++ install-jenkins-ubuntu.sh
@@ -0,0 +1,15 @@
+# # Install Jenkins on Ubuntu 20.04.6 LTS
+
+# jenkins installation
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+sudo apt install -y openjdk-17-jre
+sudo apt-get install -y jenkins
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword

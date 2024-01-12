595c3ed1dc5c6279ff974b849366a8d66f51c3a1
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-01-12 06:56:14 +0000

    Update build_agent.sh

--- build_agent.sh
+++ build_agent.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
+##  Code for the worker : In the worker worspacee
+##
+ls -l /var/lib
+sudo mkdir /var/lib/jenkins
+sudo useradd -d /var/lib/jenkins jenkins
 sudo chown jenkins:jenkins /var/lib/jenkins
 
-ls -l /var/lib/ | grep jenkins
+sudo mkdir /var/lib/jenkins/.ssh
 ssh-keygen
 
-sudo mkdir /var/lib/jenkins/.ssh
+ls -l /var/lib/ | grep jenkins
 cat ~/.ssh/id_rsa.pub
 
 sudo cat ~/.ssh/id_rsa.pub | sudo tee -a /var/lib/jenkins/.ssh/authorized_keys > /dev/null
 cat /var/lib/jenkins/.ssh/authorized_keys
 
-sudo apt-get update
-sudo apt install -y openjdk-11-jre-headless
-
 cat ~/.ssh/id_rsa
+exit 
+
+
+
+
+
+
+#!/bin/bash
+
+# # # In the master workspace
+sudo mkdir /var/lib/jenkins/.ssh
+sudo cp ~/.ssh/known_hosts /var/lib/jenkins/.ssh
+
+
 

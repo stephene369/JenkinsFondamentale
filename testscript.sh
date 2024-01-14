9b40520d72460ab2c026de81107527d58894110e
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-01-14 12:21:57 +0000

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo "THIS script is from github"
+echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
-echo "$NUMBER" 
+echo "$NUMBER"
+
 
 echo "To you all: \nYou will see how cloud compuiting will make me rich."

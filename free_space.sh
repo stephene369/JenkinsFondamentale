1a43e9040a7cc004d60789982ff298e5ca20ab17
stephene369 <81186886+stephene369@users.noreply.github.com>
2024-01-18 18:19:24 +0000

    Update free_space.sh

--- free_space.sh
+++ free_space.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Clean Up Temporary Files
-find /tmp -type f -mtime +7 -exec rm {} \;
+sudo find /tmp -type f -mtime +7 -exec rm {} \;
 
 ## Remove Old Log Files:
 sudo find /var/log -type f -name '*.gz' -delete
 
 ## Check for Large Files:
-find /tmp -type f -size +100M -exec rm {} \;
+sudo find /tmp -type f -size +100M -exec rm {} \;
 
 ## Verify Disk Space:
-df -h
+sudo df -h
 
 ##Check for Zombie Processes:
-ps aux | grep 'Z'
+sudo ps aux | grep 'Z'
 
